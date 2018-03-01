830adbf42d153face0f4fc8e6328ccf81f8e5780
Kanakam Teja <kmhx9@mst.edu>
2018-03-01 01:18:43 -0600

    Machine Learning approach for Cube Detectiom

---
+++ Bayesian Cube Detection/argosvision/pbuild/CMakeFiles/ArgosVision.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ArgosVision.dir/src/Modules/ArgosVision/ArgosVision.C.o"
+  "ArgosVision.pdb"
+  "ArgosVision.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArgosVision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
